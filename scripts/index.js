c7299cb526f9c69ba5073d61c778fcfc4059cb7f
Vítor Jungles <vitorcamargojungles@gmail.com>
2021-04-18 23:41:15 -0300

    More updates

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+var span = document.createElement('span');
+var Phrases = ['Front-end developer', 'Technology enthusiast', 'Programmer', 'Coffee'];
+var CurrentString = '';
+var PhrasesIndex = 0, CurrentStringIndex = 0;
+
+const DigitsAnimation = () => {
+  CurrentString = Phrases[PhrasesIndex].split('');
+  var FirstInterval = setInterval(function AddDigits() {
+    if (span.textContent != CurrentString.join('')) {
+      span.textContent += CurrentString[CurrentStringIndex];
+      CurrentStringIndex += 1;
+    } else {
+      clearInterval(FirstInterval);
+      PhrasesIndex == Phrases.length - 1 ? PhrasesIndex = 0 : PhrasesIndex += 1;
+      CurrentStringIndex = 0;
+      document.querySelectorAll('span')[1].classList = 'opacity-animation';
+      setTimeout(function () {
+        document.querySelectorAll('span')[1].classList = 'normal-opacity';
+        var SecondInterval = setInterval(function RemoveDigits() {
+          if (span.textContent != '') {
+            span.textContent = span.textContent.substring(0, span.textContent.length - 1);
+          } else {
+            clearInterval(SecondInterval);
+            DigitsAnimation();
+          };
+        }, 100);
+      }, 3000);
+    };
+    if (document.querySelector('header').querySelectorAll('span').length < 2) {
+      document.querySelector('span').before(span);
+    };
+  }, 100);
+};
+
+DigitsAnimation();
