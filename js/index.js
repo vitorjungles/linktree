ed3f1c7f2cbe1b73b322029ad4023ea68acb932c
Vítor Jungles <vitorcamargojungles@gmail.com>
2021-04-27 16:43:56 -0300

    Clean the code

--- js/index.js
+++ js/index.js
@@ -1,34 +1,37 @@
 let span = document.createElement('span');
-let currentString = '';
-let phrasesIndex = 0, currentStringIndex = 0;
+let phrasesIndex = 0, currentStringIndex = -1;
 
 const phrases = ['Front-end developer', 'Technology enthusiast', 'Programmer', 'Coffee'];
 
 span.classList = 'digits';
 
 const digitsAnimation = () => {
-  currentString = phrases[phrasesIndex].split('');
   let firstInterval = setInterval(() => {
-    if (span.textContent != currentString.join('')) {
-      span.textContent += currentString[currentStringIndex];
-      currentStringIndex += 1;
+    if (span.textContent != phrases[phrasesIndex]) {
+      span.textContent += phrases[phrasesIndex][currentStringIndex += 1];
     } else {
       clearInterval(firstInterval);
+
       phrasesIndex == phrases.length - 1 ? phrasesIndex = 0 : phrasesIndex += 1;
-      currentStringIndex = 0;
+
+      currentStringIndex = -1;
+
       document.querySelectorAll('span')[1].classList = 'digits opacity-animation';
+
       setTimeout(() => {
         document.querySelectorAll('span')[1].classList = 'digits normal-opacity';
+
         let secondInterval = setInterval(() => {
-          if (span.textContent != '') {
-            span.textContent = span.textContent.substring(0, span.textContent.length - 1);
-          } else {
+          span.textContent = span.textContent.substring(0, span.textContent.length - 1);
+          if (span.textContent == '') {
             clearInterval(secondInterval);
+
             digitsAnimation();
           };
         }, 100);
       }, 3000);
     };
+
     if (document.querySelectorAll('span').length < 5) {
       document.querySelector('span').before(span);
     };
